203d10ae10bf08ee6d73a1113374185e65fcbdbd
“nudt_ljn@163.com” <nudt_ljn@163.com>
2024-10-16 09:22:33 +0800

    add matadd test

--- sim/testbench.sv
+++ sim/testbench.sv
@@ -9,6 +9,8 @@
  *
  */
 
+// `define MATMUL
+
 `timescale 1ns/1ps
 module Testbench_wrapper(
 );
@@ -102,62 +104,113 @@ module Testbench_wrapper(
     #400000 $finish;
   end
 
-  reg [15:0] program_mem[31:0];
-  reg [ 7:0] data_mem[31:0];
+  reg   [31:0][15:0]  program_mem;
+  reg   [3:0][31:0][ 7:0]  data_mem;
+  reg                 start_oneTime, device_control_write_oneTime;
+  wire  [15:0]        w_program_mem_read_data = program_mem[program_mem_read_address[4:0]];
 
   always_ff @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
-      program_mem[0] = 'b0101000011011110; // MUL R0, %blockIdx, %blockDim
-      program_mem[1] = 'b0011000000001111; // ADD R0, R0, %threadIdx         ; i = blockIdx * blockDim + threadIdx
-      program_mem[2] = 'b1001000100000001; // CONST R1, #1                   ; increment
-      program_mem[3] = 'b1001001000000010; // CONST R2, #2                   ; N (matrix inner dimension)
-      program_mem[4] = 'b1001001100000000; // CONST R3, #0                   ; baseA (matrix A base address)
-      program_mem[5] = 'b1001010000000100; // CONST R4, #4                   ; baseB (matrix B base address)
-      program_mem[6] = 'b1001010100001000; // CONST R5, #8                   ; baseC (matrix C base address)
-      program_mem[7] = 'b0110011000000010; // DIV R6, R0, R2                 ; row = i // N
-      program_mem[8] = 'b0101011101100010; // MUL R7, R6, R2
-      program_mem[9] = 'b0100011100000111; // SUB R7, R0, R7                 ; col = i % N
-      program_mem[10]= 'b1001100000000000; // CONST R8, #0                   ; acc = 0
-      program_mem[11]= 'b1001100100000000; // CONST R9, #0                   ; k = 0
-                                           // LOOP:
-      program_mem[12]= 'b0101101001100010; //   MUL R10, R6, R2
-      program_mem[13]= 'b0011101010101001; //   ADD R10, R10, R9
-      program_mem[14]= 'b0011101010100011; //   ADD R10, R10, R3             ; addr(A[i]) = row * N + k + baseA
-      program_mem[15]= 'b0111101010100000; //   LDR R10, R10                 ; load A[i] from global memory
-      program_mem[16]= 'b0101101110010010; //   MUL R11, R9, R2
-      program_mem[17]= 'b0011101110110111; //   ADD R11, R11, R7
-      program_mem[18]= 'b0011101110110100; //   ADD R11, R11, R4             ; addr(B[i]) = k * N + col + baseB
-      program_mem[19]= 'b0111101110110000; //   LDR R11, R11                 ; load B[i] from global memory
-      program_mem[20]= 'b0101110010101011; //   MUL R12, R10, R11
-      program_mem[21]= 'b0011100010001100; //   ADD R8, R8, R12              ; acc = acc + A[i] * B[i]
-      program_mem[22]= 'b0011100110010001; //   ADD R9, R9, R1               ; increment k
-      program_mem[23]= 'b0010000010010010; //   CMP R9, R2
-      program_mem[24]= 'b0001100000001100; //   BRn LOOP                     ; loop while k < N
-      program_mem[25]= 'b0011100101010000; // ADD R9, R5, R0                 ; addr(C[i]) = baseC + i 
-      program_mem[26]= 'b1000000010011000; // STR R9, R8                     ; store C[i] in global memory
-      program_mem[27]= 'b1111000000000000; // RET 
-      data_mem[0]     = 'd1;
-      data_mem[1]     = 'd2; 
-      data_mem[2]     = 'd3; 
-      data_mem[3]     = 'd4;
-      data_mem[4]     = 'd1;
-      data_mem[5]     = 'd2; 
-      data_mem[6]     = 'd3; 
-      data_mem[7]     = 'd4; 
-      device_control_write_enable = '0;
-      start           = '0;
+      `ifdef MATMUL
+        //*matmul
+        program_mem[0]  <= 'b0101000011011110; // MUL R0, %blockIdx, %blockDim
+        program_mem[1]  <= 'b0011000000001111; // ADD R0, R0, %threadIdx         ; i = blockIdx * blockDim + threadIdx
+        program_mem[2]  <= 'b1001000100000001; // CONST R1, #1                   ; increment
+        program_mem[3]  <= 'b1001001000000010; // CONST R2, #2                   ; N (matrix inner dimension)
+        program_mem[4]  <= 'b1001001100000000; // CONST R3, #0                   ; baseA (matrix A base address)
+        program_mem[5]  <= 'b1001010000000100; // CONST R4, #4                   ; baseB (matrix B base address)
+        program_mem[6]  <= 'b1001010100001000; // CONST R5, #8                   ; baseC (matrix C base address)
+        program_mem[7]  <= 'b0110011000000010; // DIV R6, R0, R2                 ; row = i // N
+        program_mem[8]  <= 'b0101011101100010; // MUL R7, R6, R2
+        program_mem[9]  <= 'b0100011100000111; // SUB R7, R0, R7                 ; col = i % N
+        program_mem[10] <= 'b1001100000000000; // CONST R8, #0                   ; acc = 0
+        program_mem[11] <= 'b1001100100000000; // CONST R9, #0                   ; k = 0
+                                             // LOOP:
+        program_mem[12] <= 'b0101101001100010; //   MUL R10, R6, R2
+        program_mem[13] <= 'b0011101010101001; //   ADD R10, R10, R9
+        program_mem[14] <= 'b0011101010100011; //   ADD R10, R10, R3             ; addr(A[i]) = row * N + k + baseA
+        program_mem[15] <= 'b0111101010100000; //   LDR R10, R10                 ; load A[i] from global memory
+        program_mem[16] <= 'b0101101110010010; //   MUL R11, R9, R2
+        program_mem[17] <= 'b0011101110110111; //   ADD R11, R11, R7
+        program_mem[18] <= 'b0011101110110100; //   ADD R11, R11, R4             ; addr(B[i]) = k * N + col + baseB
+        program_mem[19] <= 'b0111101110110000; //   LDR R11, R11                 ; load B[i] from global memory
+        program_mem[20] <= 'b0101110010101011; //   MUL R12, R10, R11
+        program_mem[21] <= 'b0011100010001100; //   ADD R8, R8, R12              ; acc = acc + A[i] * B[i]
+        program_mem[22] <= 'b0011100110010001; //   ADD R9, R9, R1               ; increment k
+        program_mem[23] <= 'b0010000010010010; //   CMP R9, R2
+        program_mem[24] <= 'b0001100000001100; //   BRn LOOP                     ; loop while k < N
+        program_mem[25] <= 'b0011100101010000; // ADD R9, R5, R0                 ; addr(C[i]) = baseC + i 
+        program_mem[26] <= 'b1000000010011000; // STR R9, R8                     ; store C[i] in global memory
+        program_mem[27] <= 'b1111000000000000; // RET 
+
+        for(integer i=0; i<4; i=i+1) begin
+          data_mem[i][0]     <= 'd1;
+          data_mem[i][1]     <= 'd2; 
+          data_mem[i][2]     <= 'd3; 
+          data_mem[i][3]     <= 'd4;
+          data_mem[i][4]     <= 'd1;
+          data_mem[i][5]     <= 'd2; 
+          data_mem[i][6]     <= 'd3; 
+          data_mem[i][7]     <= 'd4; 
+        end
+      `else
+        //* matadd
+        program_mem[0]  <= 'b0101000011011110; // MUL R0, %blockIdx, %blockDim
+        program_mem[1]  <= 'b0011000000001111; // ADD R0, R0, %threadIdx         ; i = blockIdx * blockDim + threadIdx
+        program_mem[2]  <= 'b1001000100000000; // CONST R1, #0                   ; baseA (matrix A base address)
+        program_mem[3]  <= 'b1001001000001000; // CONST R2, #8                   ; baseB (matrix B base address)
+        program_mem[4]  <= 'b1001001100010000; // CONST R3, #16                  ; baseC (matrix C base address)
+        program_mem[5]  <= 'b0011010000010000; // ADD R4, R1, R0                 ; addr(A[i]) = baseA + i
+        program_mem[6]  <= 'b0111010001000000; // LDR R4, R4                     ; load A[i] from global memory
+        program_mem[7]  <= 'b0011010100100000; // ADD R5, R2, R0                 ; addr(B[i]) = baseB + i
+        program_mem[8]  <= 'b0111010101010000; // LDR R5, R5                     ; load B[i] from global memory
+        program_mem[9]  <= 'b0011011001000101; // ADD R6, R4, R5                 ; C[i] = A[i] + B[i]
+        program_mem[10] <= 'b0011011100110000; // ADD R7, R3, R0                 ; addr(C[i]) = baseC + i
+        program_mem[11] <= 'b1000000001110110; // STR R7, R6                     ; store C[i] in global memory
+        program_mem[12] <= 'b1111000000000000; // RET                            ; end of kernel 
+        
+        for(integer i=0; i<4; i=i+1) begin
+          data_mem[i][0]     <= 'd0;
+          data_mem[i][1]     <= 'd1; 
+          data_mem[i][2]     <= 'd2; 
+          data_mem[i][3]     <= 'd3;
+          data_mem[i][4]     <= 'd4;
+          data_mem[i][5]     <= 'd5; 
+          data_mem[i][6]     <= 'd6; 
+          data_mem[i][7]     <= 'd7; 
+          data_mem[i][8]     <= 'd0;
+          data_mem[i][9]     <= 'd1; 
+          data_mem[i][10]    <= 'd2; 
+          data_mem[i][11]    <= 'd3;
+          data_mem[i][12]    <= 'd4;
+          data_mem[i][13]    <= 'd5; 
+          data_mem[i][14]    <= 'd6; 
+          data_mem[i][15]    <= 'd7; 
+        end
+      `endif
+
+      device_control_write_enable <= '0;
+      device_control_data <= 8;
+      start           <= '0;
+      start_oneTime   <= '0;
+      device_control_write_oneTime <= '0; 
     end else begin
-      start           = 1;
+      device_control_write_enable <= ~device_control_write_oneTime;
+      device_control_write_oneTime<= 1;
+      start           <= ~start_oneTime & device_control_write_oneTime;
+      start_oneTime   <= device_control_write_oneTime;
       program_mem_read_ready  <= program_mem_read_valid;
       if(program_mem_read_valid)
-        program_mem_read_data <= program_mem[program_mem_read_address[4:0]];
+        program_mem_read_data <= program_mem[program_mem_read_address[0][4:0]];
 
       data_mem_read_ready     <= data_mem_read_valid;
-      if(data_mem_read_valid)
-        data_mem_read_data    <= data_mem[data_mem_read_address[4:0]];
       data_mem_write_ready    <= data_mem_write_valid;
-      if(data_mem_read_valid)
-        data_mem[data_mem_write_address[4:0]]    <= data_mem_write_data;
+      for(integer i=0; i<4; i=i+1) begin
+        if(data_mem_read_valid[i])
+          data_mem_read_data[i]    <= data_mem[i][data_mem_read_address[i][4:0]];
+        if(data_mem_write_valid[i])
+          data_mem[i][data_mem_write_address[i][4:0]]    <= data_mem_write_data[i];
+      end
     end
   end
   
